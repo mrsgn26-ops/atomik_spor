89c76ce84acf8bc1a6ac8a5f598ab18656479889
mrsgn26 <mrsgn26@gmail.com>
2025-10-14 20:37:20 +0300

    lk Kurulum: Temel uygulama iskeleti

---
+++ android/app/src/main/kotlin/com/example/atomik_spor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atomik_spor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      onGenerateTitle: (context) {
+        return AppLocalizations.of(context)!.appTitle;
+      },
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.appTitle),
+      ),
+      body: const Center(
+        child: Text(
+          'Uygulamamızın gövdesi buraya gelecek.',
+        ),
+      ),
+    );
+  }
+}
