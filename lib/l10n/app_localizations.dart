54f5318bcb4d33d7a7441569e4cfa89dad82813c
Adın Soyadın <mail@ornek.com>
2025-10-16 13:56:26 +0300

    Açıklayıcı bir mesaj

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -10,7 +10,57 @@ import 'app_localizations_tr.dart';
 
 // ignore_for_file: type=lint
 
-/// Provides localized strings for the application.
+/// Callers can lookup localized strings with an instance of AppLocalizations
+/// returned by `AppLocalizations.of(context)`.
+///
+/// Applications need to include `AppLocalizations.delegate()` in their app's
+/// `localizationDelegates` list, and the locales they support in the app's
+/// `supportedLocales` list. For example:
+///
+/// ```dart
+/// import 'l10n/app_localizations.dart';
+///
+/// return MaterialApp(
+///   localizationsDelegates: AppLocalizations.localizationsDelegates,
+///   supportedLocales: AppLocalizations.supportedLocales,
+///   home: MyApplicationHome(),
+/// );
+/// ```
+///
+/// ## Update pubspec.yaml
+///
+/// Please make sure to update your pubspec.yaml to include the following
+/// packages:
+///
+/// ```yaml
+/// dependencies:
+///   # Internationalization support.
+///   flutter_localizations:
+///     sdk: flutter
+///   intl: any # Use the pinned version from flutter_localizations
+///
+///   # Rest of dependencies
+/// ```
+///
+/// ## iOS Applications
+///
+/// iOS applications define key application metadata, including supported
+/// locales, in an Info.plist file that is built into the application bundle.
+/// To configure the locales supported by your app, you’ll need to edit this
+/// file.
+///
+/// First, open your project’s ios/Runner.xcworkspace Xcode workspace file.
+/// Then, in the Project Navigator, open the Info.plist file under the Runner
+/// project’s Runner folder.
+///
+/// Next, select the Information Property List item, select Add Item from the
+/// Editor menu, then select Localizations from the pop-up menu.
+///
+/// Select and expand the newly-created Localizations item then, for each
+/// locale your application supports, add a new item and select the locale
+/// you wish to add from the pop-up menu in the Value field. This list should
+/// be consistent with the languages listed in the AppLocalizations.supportedLocales
+/// property.
 abstract class AppLocalizations {
   AppLocalizations(String locale)
       : localeName = intl.Intl.canonicalizedLocale(locale.toString());
@@ -24,6 +74,16 @@ abstract class AppLocalizations {
   static const LocalizationsDelegate<AppLocalizations> delegate =
       _AppLocalizationsDelegate();
 
+  /// A list of this localizations delegate along with the default localizations
+  /// delegates.
+  ///
+  /// Returns a list of localizations delegates containing this delegate along with
+  /// GlobalMaterialLocalizations.delegate, GlobalCupertinoLocalizations.delegate,
+  /// and GlobalWidgetsLocalizations.delegate.
+  ///
+  /// Additional delegates can be added by appending to this list in
+  /// MaterialApp. This list does not have to be used at all if a custom list
+  /// of delegates is preferred or required.
   static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
       <LocalizationsDelegate<dynamic>>[
     delegate,
@@ -32,19 +92,64 @@ abstract class AppLocalizations {
     GlobalWidgetsLocalizations.delegate,
   ];
 
+  /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
     Locale('en'),
-    Locale('tr'),
+    Locale('tr')
   ];
 
+  /// No description provided for @appTitle.
+  ///
+  /// In en, this message translates to:
+  /// **'Atomik Spor'**
   String get appTitle;
+
+  /// No description provided for @showCalendarButton.
+  ///
+  /// In en, this message translates to:
+  /// **'Show My Calendar'**
   String get showCalendarButton;
+
+  /// No description provided for @habitTrackerTitle.
+  ///
+  /// In en, this message translates to:
+  /// **'Habit Calendar'**
   String get habitTrackerTitle;
+
+  /// No description provided for @habitListTitle.
+  ///
+  /// In en, this message translates to:
+  /// **'Daily Habits'**
   String get habitListTitle;
+
+  /// No description provided for @habitCompletionSummary.
+  ///
+  /// In en, this message translates to:
+  /// **'{completed} of {total} habits completed'**
   String habitCompletionSummary(int completed, int total);
+
+  /// No description provided for @habitInstructions.
+  ///
+  /// In en, this message translates to:
+  /// **'Tap a habit to mark it as done.'**
   String get habitInstructions;
+
+  /// No description provided for @habitMorningStretch.
+  ///
+  /// In en, this message translates to:
+  /// **'Morning stretch'**
   String get habitMorningStretch;
+
+  /// No description provided for @habitReading.
+  ///
+  /// In en, this message translates to:
+  /// **'Read for 10 minutes'**
   String get habitReading;
+
+  /// No description provided for @habitHydrate.
+  ///
+  /// In en, this message translates to:
+  /// **'Drink a glass of water'**
   String get habitHydrate;
 }
 
@@ -66,6 +171,7 @@ class _AppLocalizationsDelegate
 }
 
 AppLocalizations lookupAppLocalizations(Locale locale) {
+  // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
     case 'en':
       return AppLocalizationsEn();
@@ -74,9 +180,8 @@ AppLocalizations lookupAppLocalizations(Locale locale) {
   }
 
   throw FlutterError(
-    'AppLocalizations.delegate failed to load unsupported locale "$locale".\n'
-    'This is likely an issue with the localizations generation tool. '
-    'Please file an issue on GitHub with a reproducible sample app and the '
-    'gen-l10n configuration that was used.',
-  );
+      'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
+      'an issue with the localizations generation tool. Please file an issue '
+      'on GitHub with a reproducible sample app and the gen-l10n configuration '
+      'that was used.');
 }
