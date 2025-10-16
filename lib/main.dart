a84292f66542fa4bc1e8c96ee365605bb9fe3271
mrsgn26-ops <mrsgn26@gmail.com>
2025-10-16 14:18:36 +0300

    Use generated localizations package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'habit_tracker_screen.dart';
-import 'l10n/app_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
